b377af026865d2d4abb755412b93db8a3013b070
lare <laresamdeola@gmail.com>
2023-01-07 08:10:49 +0000

    Update 3 files and another useReduce practise

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import ReduceExample1 from "./components/useReducer/ReduceExample1";
+import ReduceExample2 from "./components/useReducer/ReduceExample2";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
-root.render(<ReduceExample1 />);
+root.render(<ReduceExample2 />);
