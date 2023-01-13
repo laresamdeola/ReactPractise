6f52ea2263039e0b6e1c90da17bcc99c3bee189b
lare <laresamdeola@gmail.com>
2023-01-13 13:05:25 +0000

    Update 5 files, including the inline, internal using objects and external style sheets

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { Components } from "./components/lessons/components";
+import { CssExternal } from "./components/lessons/CssExternal";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
-root.render(<Components />);
+root.render(<CssExternal />);
