913e1af1d3d8ac450abc4b11039b7432279e9be9
lare <laresamdeola@gmail.com>
2023-01-14 09:34:33 +0000

    Update propsdestructuring.jsx and index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { Poets } from "./components/lessons/props2";
+import { Poets2 } from "./components/lessons/propsdestructuring";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
-root.render(<Poets />);
+root.render(<Poets2 />);
