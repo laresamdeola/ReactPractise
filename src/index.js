fbdc40a09d0901112b0379581403352ed6337bfb
lare <laresamdeola@gmail.com>
2023-01-14 07:52:16 +0000

    Update 5 files including props, using external data as props, conditional rendering

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { CssExternal } from "./components/lessons/CssExternal";
+import { Poets } from "./components/lessons/props2";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
-root.render(<CssExternal />);
+root.render(<Poets />);
