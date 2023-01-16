fe3d81e163967ce90738c2019da718758deee303
lare <laresamdeola@gmail.com>
2023-01-16 11:49:51 +0000

    Another example using useState hook and conditional rendering of a state variable

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { Simple } from "./components/lessons/usestatesimple";
+import { Affection } from "./components/lessons/usestatebasics";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
-root.render(<Simple />);
+root.render(<Affection />);
