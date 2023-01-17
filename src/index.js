b188b3bf25a9c4d8d227f39482ffb3bc6f36e3c2
lare <laresamdeola@gmail.com>
2023-01-17 13:07:09 +0000

    useState example with multiple state values

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { ObjectExample } from "./components/lessons/usestateobject";
+import { MultipleState } from "./components/lessons/usestatemultiplestatevalues";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
-root.render(<ObjectExample />);
+root.render(<MultipleState />);
