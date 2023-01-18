6301d9c8e35f97dc787622bd08e1c959bc79dda0
lare <laresamdeola@gmail.com>
2023-01-18 09:50:41 +0000

    counter application using useState hook

---
+++ src/components/lessons/counter.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        +
+      </button>
+      <button
+        onClick={() => {
+          setCount(count - 1);
+        }}
+      >
+        -
+      </button>
+      <button
+        onClick={() => {
+          setCount(0);
+        }}
+      >
+        reset
+      </button>
+    </div>
+  );
+};
+
+export { Counter };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { MultipleState } from "./components/lessons/usestatemultiplestatevalues";
+import { Counter } from "./components/lessons/counter";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
-root.render(<MultipleState />);
+root.render(<Counter />);
