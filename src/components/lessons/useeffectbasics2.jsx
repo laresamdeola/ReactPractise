5063ccac6a28b1e45b0b9f1826359ea332d7ac58
lare <laresamdeola@gmail.com>
2023-01-22 09:34:20 +0000

    a useEffect basic example updating the document title while adding items to the count state variable.

--- src/components/lessons/useeffectbasics2.jsx
+++ src/components/lessons/useeffectbasics2.jsx
@@ -1,15 +1,20 @@
 import React, { useState, useEffect } from "react";
 
 const EffectBasics2 = () => {
-  const age = 10;
-  const display = () => {
-    alert(age);
+  const [count, setCount] = useState(0);
+
+  const add = () => {
+    setCount(count + 1);
   };
+
+  useEffect(() => {
+    document.title = `Basics(${count})`;
+  }, [count]);
 
   return (
     <>
-      <h1>{age}</h1>
-      <button onClick={() => display()}>display</button>
+      <h1>{count}</h1>
+      <button onClick={() => add()}>+</button>
     </>
   );
 };
