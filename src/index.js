2772ad4b50607abb8eff2c2f18f94f36d0e0eaef
lare <laresamdeola@gmail.com>
2023-01-23 07:37:35 +0000

    example of using useEffect with conditionals

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { EffectBasics2 } from "./components/lessons/useeffectbasics2";
+import { Conditional } from "./components/lessons/useeffectconditional";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
-root.render(<EffectBasics2 />);
+root.render(<Conditional />);
