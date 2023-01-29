8403f023c7f99dc891659151d7e2f8bf354aa8d0
lare <laresamdeola@gmail.com>
2023-01-29 08:12:46 +0000

    Update 5 files and useEffect to fetch data from a get endpoint of an API

---
+++ src/components/lessons/useeffectfetchdata.jsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+import { useFetch } from "../customhooks/useFetch";
+
+const FetchData = () => {
+  const url = "https://api.github.com/users";
+  const github = useFetch(url);
+  const { isLoading, data } = github;
+
+  return (
+    <>
+      {data.map((datum) => {
+        const { id, login, avatar_url } = datum;
+        return (
+          <div key={id}>
+            <h3>{login}</h3>
+            <img src={avatar_url} alt={login} />
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export { FetchData };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { Conditional4 } from "./components/lessons/useeffectconditional4";
+import { FetchData } from "./components/lessons/useeffectfetchdata";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
-root.render(<Conditional4 />);
+root.render(<FetchData />);
