13837a478acf91ff64c9a95866d441c6a32f47b4
lare <laresamdeola@gmail.com>
2023-01-29 06:47:29 +0000

    Update 2 files, anothe useEffect example

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { Conditional2 } from "./components/lessons/useeffectconditional2";
+import { Conditional4 } from "./components/lessons/useeffectconditional4";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
-root.render(<Conditional2 />);
+root.render(<Conditional4 />);
