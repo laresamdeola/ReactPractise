b7d3ef32cf30bd9287e433cee2ba6ae8d3dcf8d8
lare <laresamdeola@gmail.com>
2023-01-31 16:15:16 +0000

    Update ternaryoperator.jsx

--- src/components/lessons/ternaryoperator.jsx
+++ src/components/lessons/ternaryoperator.jsx
@@ -1,23 +1,17 @@
 import React, { useState } from "react";
 
 const Ternary = () => {
-  const [name, setName] = useState("Chikam");
-  const [affection, setAffection] = useState("");
-
-  const handleAffection = () => {
-    setAffection("I love you");
-    affection === "I love you"
-      ? alert(`${name} you're so lucky`)
-      : alert(`Hmm...`);
-  };
+  const [name, setName] = useState("");
 
   return (
     <div>
-      <h1>{name}</h1>
-      <h3>{affection}</h3>
-      <button type="button" onClick={handleAffection}>
-        Show Affection
-      </button>
+      <h3 style={{ color: name === "Chikam" ? "red" : "blue" }}>{name}</h3>
+      <input
+        type="text"
+        name="name"
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+      />
     </div>
   );
 };
