61b6a956b7485d679cd9a6428c943a872e36768d
lare <laresamdeola@gmail.com>
2023-02-04 09:46:05 +0000

    Update 6 files and also useRef example

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { ControlledInputs } from "./components/lessons/controlledinputs";
+import { RefExample1 } from "./components/lessons/useref";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
-root.render(<ControlledInputs />);
+root.render(<RefExample1 />);
