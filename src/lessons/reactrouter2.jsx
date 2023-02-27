d7b5b527ead054d5e5ab298f41fab702f05a6dc6
lare <laresamdeola@gmail.com>
2023-02-27 15:06:03 +0000

    react router practise

---
+++ src/lessons/reactrouter2.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Outlet,
+} from "react-router-dom";
+
+const ReactRouter = () => {
+  return <h1>Hello World!</h1>;
+};
