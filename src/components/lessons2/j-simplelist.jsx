ed7c1bde655cdf04f996757c79b024da42cc3eae
lare <laresamdeola@gmail.com>
2023-03-22 08:39:33 +0000

    loop through simple list using javascript Map higher order function.

--- src/components/lessons2/j-simplelist.jsx
+++ src/components/lessons2/j-simplelist.jsx
@@ -1,1 +1,40 @@
 // Continue from here
+// use map here - create a list then loop through using map
+
+import React from "react";
+
+const SimpleList = () => {
+  const data = [
+    {
+      id: 1,
+      name: "Lare",
+      age: 10,
+    },
+    {
+      id: 2,
+      name: "Sholape",
+      age: 74,
+    },
+    {
+      id: 3,
+      name: " Damilola",
+      age: 43,
+    },
+  ];
+
+  return (
+    <div>
+      {data.map((datum) => {
+        const { id, name, age } = datum;
+        return (
+          <section key={id}>
+            <h3>{name}</h3>
+            <h4>{age}</h4>
+          </section>
+        );
+      })}
+    </div>
+  );
+};
+
+export { SimpleList };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { PropsChildren } from "./components/lessons2/i-propschildren";
+import { SimpleList } from "./components/lessons2/j-simplelist";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
-root.render(<PropsChildren />);
+root.render(<SimpleList />);
