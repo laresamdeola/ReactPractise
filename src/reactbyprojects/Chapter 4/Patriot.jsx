b3d836317c9f8b123d48f2fca9f6218f71f023c1
lare <laresamdeola@gmail.com>
2023-04-03 06:47:44 +0000

    Update 2 files - Chapter 4 - Patriots

---
+++ src/reactbyprojects/Chapter 4/Patriot.jsx
@@ -0,0 +1,7 @@
+// 1. A Quiz Website of ten questions
+// 2. the questions would be in a from
+// 3. Create a Score State that updates as one either answers
+// correctly or incorrectly
+// 4. Display this Score state to the user when the quiz is over
+// 5. One can display an alert also
+// 6. Save the score in a txt file
