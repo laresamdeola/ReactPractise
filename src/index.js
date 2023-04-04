afaba2ae34595f1b62de8819de94db7538d723fd
lare <laresamdeola@gmail.com>
2023-04-04 10:31:55 +0000

    Update index.js and Patriot.jsx - Patriot Quiz App

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { HelloWorld3 } from "./reactbyprojects/Chapter 3/HelloWorld3";
+import { Patriot } from "./reactbyprojects/Chapter 4/Patriot";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
-root.render(<HelloWorld3 />);
+root.render(<Patriot />);
--- src/reactbyprojects/Chapter 4/Patriot.jsx
+++ src/reactbyprojects/Chapter 4/Patriot.jsx
@@ -1,7 +1,44 @@
 // 1. A Quiz Website of ten questions
-// 2. the questions would be in a from
+// 2. the questions would be in a form
 // 3. Create a Score State that updates as one either answers
 // correctly or incorrectly
 // 4. Display this Score state to the user when the quiz is over
 // 5. One can display an alert also
 // 6. Save the score in a txt file
+
+import React, { useState } from "react";
+
+const Patriot = () => {
+    var score = 0;
+    const [questionOne, setQuestionOne] = useState("");
+    
+    const handleQuiz = (event) => {
+        event.preventDefault();
+        if(questionOne.toLowerCase() == "abuja"){
+            score += 1;
+            console.log("You got it");
+        } else {
+            score = 0;
+            console.log("You missed it", score);
+        }
+        console.log(score);
+    }
+    
+    return (
+        <>
+            <form onSubmit={handleQuiz}>
+                <label>Question 1: What is the capital of Nigeria?</label>
+                <input 
+                    type="text"
+                    required
+                    name="questionOne"
+                    value={questionOne}
+                    onChange={(event) => setQuestionOne(event.target.value)}
+                />
+                <button type="submit">Finish</button>
+            </form>
+        </>
+    );
+}
+
+export { Patriot };
