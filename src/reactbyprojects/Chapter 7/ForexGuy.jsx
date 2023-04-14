b76476c10a2b94aaa396649f557a24223f99341d
lare <laresamdeola@gmail.com>
2023-04-14 03:44:38 +0000

    Update ForexGuy.jsx and reactbyprojects.txt

--- src/reactbyprojects/Chapter 7/ForexGuy.jsx
+++ src/reactbyprojects/Chapter 7/ForexGuy.jsx
@@ -4,19 +4,22 @@ import React, { useState, useEffect } from "react";
 
 const ForexGuy = () => {
     const [currencyRate, setCurrencyRate] = useState({});
+    
+    // State to handle null/loading state gracefully and prevent crashes
     const [fetched, setFetched] = useState(false);
     const url = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.json"
     
     const fetchCurrency = async() => {
-        const data = await fetch(url);
+        const data = await fetch(url, { "Access-Control-Allow-Origin": "no-cors" });
         const dataJson = await data.json();
         setCurrencyRate(dataJson);
         setFetched(true);
-        console.log(dataJson, Object.keys(dataJson.usd).length);
+        console.log(dataJson);
     }
     
     useEffect(() => {
         fetchCurrency();
+        //document.title = `ForexGuy - Currencies(${Object.keys(currencyRate.usd).length})`;
     }, [url]);
     
     return (
