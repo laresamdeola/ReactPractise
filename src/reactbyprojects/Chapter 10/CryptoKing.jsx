eb27168d0d8b9c5dc3afbaac7186dce1fd8f48f1
lare <laresamdeola@gmail.com>
2023-04-26 20:08:59 +0000

    Begin Chapter 10 of the reactbyprojects book

---
+++ src/reactbyprojects/Chapter 10/CryptoKing.jsx
@@ -0,0 +1,12 @@
+// 1. use the useReducer() to handle the various state values.
+
+const CryptoKing = () => {
+    
+    return (
+        <>
+            <h1>Welcome to CryptoKing!</h1>
+        </>
+    );
+};
+
+export { CryptoKing };
